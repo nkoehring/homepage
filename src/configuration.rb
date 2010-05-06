4ff68c03a1191f1cb391603fe04124406318bf2a
nkoehring <nkoehring@nkoehring.de>
2010-05-07 01:18:39 +0200

    finished now?
    Using a nice and small font in a very compatible way now – thanks to FontSquirrel and Paul Irish.
    I'm now using textile to write the contents. So I can have punctuation directly behind anchors now, yay!

---
+++ src/configuration.rb
@@ -0,0 +1,12 @@
+
+configuration.preview_server_port = 3000
+configuration.preview_server_host = "localhost"
+
+configuration.sass_options = {
+    :style => :compact
+}
+
+configuration.haml_options = {
+      :format => :html5
+}
+
