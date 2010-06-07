e571186e879747f3492e71a4d4e81a036dc0358b
Norman <nkoehring@nkoehring.de>
2010-06-07 14:46:36 +0200

    blog link restored

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 PATH=/opt/ruby1.8/bin:$PATH # just for people handling Ruby1.8 besides Ruby1.9
+archive=nkoehring.de.tbz2
+
 staticmatic build .
-tar cjf nkoehring.de.tbz2 site
-scp nkoehring.de.tbz2 nkoehring.de:~
+tar cjf $archive site
+scp $archive nkoehring.de:~
+
+echo "Upload finished… running remote deployment script…"
+ssh nkoehring.de 'sh ~/deploy_website.sh' && echo "Done." || echo "Failed… :("
+
+rm $archive
 
